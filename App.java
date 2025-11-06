20c341736b4ed744d834db3359f31bc444d8ec4a
somasekhar10 <nagasaib663@gmail.com>
2025-11-06 09:10:41 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Secure CI/CD Java World!");
+    }
+}
+
+
+// Gitleaks secrets
+// public class App {
+//     public static void main(String[] args) {
+//         // AWS keys must start with "AKIA" and be followed by 16 alphanumeric chars
+//         String aws_key = "AKIA1234567890ABCD12";
+//     }
+// }
+
+
+// checkstyle error
+// public class App {
+// public static void main(String[] args) {
+// System.out.println("Hello, Secure CI/CD Java World!") // Missing semicolon, no indentation
+// }
+// }
